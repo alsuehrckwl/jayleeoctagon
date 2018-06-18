5f6f454597fc4dca95bc3025e51608b513e91a8f
alsuehrckwl <alsuehrckwl@naver.com>
2018-06-18 17:13:53 +0900

    Update Firebase.js

--- src/shared/Firebase.js
+++ src/shared/Firebase.js
@@ -1,12 +1,7 @@
 import * as firebase from 'firebase'
 let database;
 let config = {
-    apiKey: "AIzaSyDeXY5YKhI30_V1TQGDvrgxKgmIfOY6Itw",
-    authDomain: "jayleeoctagon.firebaseapp.com",
-    databaseURL: "https://jayleeoctagon.firebaseio.com",
-    projectId: "jayleeoctagon",
-    storageBucket: "jayleeoctagon.appspot.com",
-    messagingSenderId: "931750057208"
+
 }
 
 export const fire = () => {
