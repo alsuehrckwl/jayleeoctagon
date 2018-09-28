00423118f28f433166c7fef5d9bc7a92e74b204a
jwkim88 <jwkim88@1thefull.com>
2018-09-28 16:48:56 +0900

    main 수정

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -7,6 +7,37 @@ export default class Main extends Component {
   render() {
     return (
       <div>
+        <header className="masthead text-white text-center">
+          <div className="overlay" />
+          <div className="container">
+            <div className="row">
+              <div className="col-xl-9 mx-auto">
+                <h1 className="mb-5">Welcome to the club OCTAGON!</h1>
+              </div>
+              <div className="col-md-10 col-lg-8 col-xl-7 mx-auto">
+                <h1 className="mb-5">Guest 신청 문의</h1>
+                <a
+                  href="tel:010-2888-0756"
+                  className="btn btn-block btn-lg btn-warning"
+                >
+                  010 - 2888 - 0756
+                  <br /> 클릭하면 연결됩니다.
+                </a>
+                {/* <form>
+                        <div className="form-row">
+                            <div className="col-12 col-md-9 mb-2 mb-md-0">
+                            <input type="email" className="form-control form-control-lg" placeholder="Enter your email..."/>
+                            </div>
+                            <div className="col-12 col-md-3">
+                            <button type="submit" className="btn btn-block btn-lg btn-primary">Sign up!</button>
+                            </div>
+                        </div>
+                        </form> */}
+              </div>
+            </div>
+          </div>
+        </header>
+
         <Schedule />
         <Price />
         <Reservation />
